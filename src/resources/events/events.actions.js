028343d2696b8d048a8bd7bd290a818bc0e2590a
toan.ngo <toan.ngo@transxsystems.com>
2018-11-03 00:41:06 +0700

    add actions for events route

--- src/resources/events/events.actions.js
+++ src/resources/events/events.actions.js
@@ -1,4 +1,7 @@
- const actions = {
+const List = require('../../models/list');
+const Todo = require('../../models/todo');
+
+const actions = {
     v1: {
         getList: getList,
         getListByID: getListByID,
@@ -7,22 +10,53 @@
 }
 
 function getList(req,res,next) {
-    res.status(200).json({
-        message:'Handling getListByID request'
+    List.find({ status: { $gt: 0 } })
+    .select('_id name events')
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            items: docs
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
     });
 }
 
 function getListByID (req,res,next) {
-    res.status(200).json({
-        message:'Handling getListByID request'
+    const id = req.params.listId;
+    List.findOne({_id: id, status: { $gt: 0 } })
+    .select('_id name events')
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            items: docs
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
     });
 }
 
 
 
 function getTodoByID(req,res,next) {
-    res.status(200).json({
-        message:'Handling getTodoByID request'
+    const listId = req.params.listId;
+    const todoId = req.params.todoId;
+    Todo.findOne({ _id: todoId, list: listId, status: { $gt: 0 } })
+    .select('_id name events')
+    .then(doc => {
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({ message: 'No valid entry found for provided ID' });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
     });
 }
 
