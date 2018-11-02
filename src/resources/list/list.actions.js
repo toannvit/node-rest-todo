6abcfcc19df7fc52dc502dcb4658264006b026d6
toan.ngo <toan.ngo@transxsystems.com>
2018-11-03 00:14:30 +0700

    add CRUD actions for list

--- src/resources/list/list.actions.js
+++ src/resources/list/list.actions.js
@@ -1,4 +1,5 @@
-
+const List = require('../../models/list');
+const mongoose = require('mongoose');
 const actions = {
     v1: {
         getList: getList,
@@ -10,34 +11,112 @@ const actions = {
 }
 
 function getList(req, res, next) {
-    res.status(200).json({
-        message:'Handling getList request'
+    List.find({ status: { $gt: 0 } })
+    .select('_id name status')
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            items: docs
+        })
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
     });
 }
 
 function getListByID(req, res, next) {
-    res.status(200).json({
-        message:'Handling getListByID request'
+    const id = req.params.listId;
+    List.findOne({ _id: id, status: { $gt: 0 } }).then(doc => {
+        if (doc) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({ message: 'No valid entry found for provided ID' });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
     });
 }
 
 function createList(req, res, next) {
-    res.status(200).json({
-        message:'Handling createList request'
+    const list = new List({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        events: [
+            {
+                _id: new mongoose.Types.ObjectId(),
+                name: 'listCreated',
+                when: new Date(),
+            }
+        ]
     });
+    list.save().then(doc => {
+        res.status(201).json({
+            createdResult: doc,
+            links: {
+                type: 'GET',
+                href: ''
+            }
+        })
+    }).catch(err => {
+        res.status(500).json({
+            error
+        });
+    });
+
 }
 
 function updateList(req, res, next) {
-    res.status(200).json({
-        message:'Handling updateList request'
-    });
+    const id = req.params.listId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+
+    List.update({ _id: id }, {
+        $set: updateOps,
+        $push: {
+            events: {
+                _id: new mongoose.Types.ObjectId(),
+                name: 'listUpdated',
+                when: new Date()
+            }
+        }
+    }).then(result => {
+        res.status(200).json(result);
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
+    })
+
 }
 
 
 function deleteList(req, res, next) {
-    res.status(200).json({
-        message:'Handling deleteList request'
-    });
+    const id = req.params.listId;
+    List.update({ _id: id }, {
+        $set: {
+            status: -2 //delete
+        },
+        $push: {
+            events: {
+                _id: new mongoose.Types.ObjectId(),
+                name: 'listDeleted',
+                when: new Date()
+            }
+        }
+    }).then(result => {
+        res.status(200).json(result);
+    }).catch(error => {
+        res.status(500).json({
+            error
+        });
+    })
+
+
 }
 
 
